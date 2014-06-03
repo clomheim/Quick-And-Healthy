635083d72cd100a54c97b785d686d0e761d9c93b
Chicachu <Eputsche@yahoo.com>
2014-06-02 18:27:20 -0700

    Added directions field to Recipe

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,17 +1,23 @@
 import java.util.Date;
 import java.util.List;
 
+/** 
+ * Recipe with title, category, prep and cook times, a list of ingredients and nutritional information.
+ * @author Erica
+ *
+ */
 public class Recipe {
 	private int my_recipeID;
 	private String my_recipeTitle;
-	private String my_category; 
+	private String my_category;
 	private Date my_prepTime;
 	private Date my_cookTime;
 	private List<String> my_ingredients;
+	private String my_directions;
 	private NutritionalInfo my_nutritionalInfo;
 	
 	public Recipe(int recipeID, String recipeTitle, String category, Date prepTime, 
-					Date cookTime, List<String> ingredients, NutritionalInfo nutritionalInfo) {
+					Date cookTime, List<String> ingredients, String directions, NutritionalInfo nutritionalInfo) {
 		my_recipeID = recipeID; 
 		my_category = category;
 		my_prepTime = prepTime; 
@@ -21,6 +27,16 @@ public class Recipe {
 			my_ingredients.add(s);
 		}
 		
+		my_directions = directions;
 		my_nutritionalInfo = nutritionalInfo;
+	}
+	
+	public void setNutritionalInfo(NutritionalInfo the_info) {
+		my_nutritionalInfo = the_info;
+	}
+	
+	@Override
+	public String toString() {
+		return my_recipeTitle;
 	}
 }
